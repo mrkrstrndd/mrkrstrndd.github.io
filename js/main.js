8cf16fcbed27d0af1ecd3ed434f94accc3b59b3e
Trinidad, Mark Aris M <mark.aris.m.trinidad@accenture.com>
2015-10-02 19:26:50 +0800

    first commmit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,99 @@
-var app = angular.module('app', ['ngAnimate']);
+var app = angular.module('app', ['ui.router','ngAnimate']);
 
-app.controller('headerController', function($scope){
+app.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("home");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('home', {
+      url: "/home",
+      templateUrl: "partials/home.html"
+    })
+   
+    .state('location', {
+      url: "/location",
+      templateUrl: "partials/location.html"
+    })
 
-	$scope.top = false;	
-	$scope.home = function(){
-		$scope.top = false;	
-	}
+    .state('history', {
+      url: "/history",
+      templateUrl: "partials/history.html"
+    })
+
+    .state('rates', {
+      url: "/rates",
+      templateUrl: "partials/rates.html"
+    })
+
+    .state('gallery', {
+      url: "/gallery",
+      templateUrl: "partials/gallery.html"
+    })
+
+    .state('rvsale', {
+      url: "/rvsale",
+      templateUrl: "partials/rvsale.html"
+    })
+
+    .state('rvsite', {
+      url: "/rvsite",
+      templateUrl: "partials/rvsite.html"
+    })
+
+    .state('link', {
+      url: "/link",
+      templateUrl: "partials/link.html"
+    })
+
+    .state('rules', {
+      url: "/rules",
+      templateUrl: "partials/rules.html"
+    })
+
+
+  
+});
+
+app.controller('headerController', function($scope,$state,$stateParams){
 	
-	$scope.loc = function(){
-		$scope.top = true;	
+	$scope.menuTitle = [
+		{ "name" : "Home" , "link" : "home"},
+		{ "name" : "Location-Contact", "link" : "location" },
+		{ "name" : "History" , "link" : "history"},
+		{ "name" : "Rates" , "link" : "rates"},
+		{ "name" : "Picture Gallery", "link" : "gallery" },
+		{ "name" : "RVs For Sale" , "link" : "rvsale"},
+		{ "name" : "RV Sites" , "link" : "rvsite"},
+		{ "name" : "Links", "link" : "link" },
+		{ "name" : "Park Rules" , "link" : "rules"},
+	];
+
+	$scope.rates = [
+		{ "name" : "Seasonal" , "rate" : "$1095.00"},
+		{ "name" : "Monthly (50 Amp)" , "rate" : "$390.00"},
+		{ "name" : "Monthly (30 Amp)" , "rate" : "$360.00"},
+		{ "name" : "Weekly (50 Amp)" , "rate" : "$172.00"},
+		{ "name" : "Weekly (30 Amp)" , "rate" : "$160.00"},
+		{ "name" : "Daily (50 Amp)" , "rate" : "$29.00"},
+		{ "name" : "Daily (30 Amp)" , "rate" : "$27.00"},
+	];
+
+	$scope.selected = $scope.menuTitle[0];
+	$scope.top = false;
+
+	$scope.select = function(item){
+		if(item.name === 'Home')
+			$scope.top = false;
+		else
+			$scope.top = true;
+
+		$scope.selected = item; 
+		$state.go($scope.selected.link);
 	}
-})
+
+	$scope.isActive = function(item){
+		 return $scope.selected === item;
+	}
+});
