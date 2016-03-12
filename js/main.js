37073417df4d88e8a0392835e6fcc94ff71a439f
mrkrstrndd <mrkrstrndd@gmail>
2016-03-12 21:39:31 +0800

    commit reservations

--- js/main.js
+++ js/main.js
@@ -1,43 +1,86 @@
-$(function(){
-	 $("header div.bottom-header").sticky({topSpacing:0});
-	 $("header div.bottom-header").on("sticky-start", function(){
-	 	
-	 	$(this).children("img").animate({ 
-	 		"width": "10em",
-	 		"margin-top": "0.4em",
-	 		"margin-bottom":"0.4em"
-	 	},200);
+var app = angular.module('myApp',['ui.router','slick']);
 
-	 	$(this).children("nav").animate({
-	 		"top":"2em"
-	 	},200)
-	 })
+app.controller('myController', function($scope){
+	$scope.initialize = function(){
+		 $("header div.bottom-header").sticky({topSpacing:0});
+		 $("header div.bottom-header").on("sticky-start", function(){
+		 	
+		 	$(this).children("img").animate({ 
+		 		"width": "10em",
+		 		"margin-top": "0.4em",
+		 		"margin-bottom":"0.4em"
+		 	},200);
 
-	 $("header div.bottom-header").on("sticky-end", function(){
-	 	
-	 	$(this).children("img").animate({ 
-	 		"width": "13em",
-	 		"margin-top":"1em",
-	 		"margin-bottom":"1em"
-	 	},200);
- 		$(this).children("nav").animate({
-	 		"top":"3em"
-	 	},200)
-	 })
+		 	$(this).children("nav").animate({
+		 		"top":"2em"
+		 	},200)
+		 })
 
+		 $("header div.bottom-header").on("sticky-end", function(){
+		 	
+		 	$(this).children("img").animate({ 
+		 		"width": "13em",
+		 		"margin-top":"1em",
+		 		"margin-bottom":"1em"
+		 	},200);
+	 		$(this).children("nav").animate({
+		 		"top":"3em"
+		 	},200)
+		 })	
+	}
 
-	 $('div.photos').slick({
-	 	centerMode: true,
-  		variableWidth: true,
-  		dots: true,  	
-  		autoplay: true,
- 	    autoplaySpeed: 5000,	
-  		arrow:true,
-  		
-	 });
+	$scope.customer = {
+    name: 'Naomi',
+    address: '1600 Amphitheatre'
+  };
 });
 
-var app = angular.module('myApp',['ui.router','slick']);
+app.directive('myTextbox', ['$document', function($document) {
+  return {
+    link: function(scope, element, attr) {
+    	
+
+    	element.on('keyup', hehe);
+
+    	function hehe(event){
+    		console.log(event.target.innerText);
+    	}
+
+      // var startX = 0, startY = 0, x = 0, y = 0;
+
+      // element.css({
+      //  position: 'relative',
+      //  border: '1px solid red',
+      //  backgroundColor: 'lightgrey',
+      //  cursor: 'pointer'
+      // });
+
+      // element.on('mousedown', function(event) {
+      //   // Prevent default dragging of selected content
+      //   event.preventDefault();
+      //   startX = event.pageX - x;
+      //   startY = event.pageY - y;
+      //   $document.on('mousemove', mousemove);
+      //   $document.on('mouseup', mouseup);
+      //   console.log('haha');
+      // });
+
+      // function mousemove(event) {
+      //   y = event.pageY - startY;
+      //   x = event.pageX - startX;
+      //   element.css({
+      //     top: y + 'px',
+      //     left:  x + 'px'
+      //   });
+      // }
+
+      // function mouseup() {
+      //   $document.off('mousemove', mousemove);
+      //   $document.off('mouseup', mouseup);
+      // }
+    }
+  };
+}]);
 
 app.config(function($stateProvider, $urlRouterProvider){
 
@@ -77,4 +120,11 @@ app.config(function($stateProvider, $urlRouterProvider){
 	    })
 
     })
+
+
+
+$(function(){
+	
+
+});
 
